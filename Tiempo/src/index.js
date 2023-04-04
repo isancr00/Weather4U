c0e43edcc31c4b22681a839bc7ae590c309c442b
Iván Sánchez <isancr00@estudiantes.unileon.es>
2023-04-04 16:46:00 +0200

    Petición al servicio de amanecer atardecer exitosa

--- Tiempo/src/index.js
+++ Tiempo/src/index.js
@@ -1,21 +1,51 @@
 const app = require('./app');
 var express = require('express');
-var adminRouter = express.Router()
+const https = require('https');
+var adminRouter = express.Router();
 
 adminRouter.get('/amanecerAtardecer/:coordenadas',function(req,res){
     console.log("Datos amanecer atardecer");
-    getAmanecerAtardecer(res);
+    manejadoraAmanecerAtardecer(req.params.coordenadas,res);
 });
 
 app.use(adminRouter);
 
-async function getAmanecerAtardecer(res){
+async function manejadoraAmanecerAtardecer(coordenadas,res){
+    var data;
+    var coordenadasSplit = coordenadas.split('_');
+    var latitud = coordenadasSplit[0];
+    var longitud = coordenadasSplit[1];
 
+    const url = "https://api.sunrise-sunset.org/json?lat="+ latitud +'&lng=' + longitud;
+    console.log(url);   
+
+    makeGetRequest(url, (error, data) => {
+        if (error) {
+          console.error(error);
+        } else {
+            console.log("Peticion realizada con exito")
+            res.send(data);
+        }
+      });
     
 }
 
+function makeGetRequest(url, callback) {
+  https.get(url, (response) => {
+    let data = '';
+    
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
 
+    response.on('end', () => {
+      callback(null, data);
+    });
 
+  }).on('error', (error) => {
+    callback(error, null);
+  });
+}
 
 
 async function main(){
